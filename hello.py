93dcf1bc1920bcab99685367d1fce4e22e53125f
wondo <Muche.Berhanu@metropolia.fi>
2022-08-29 15:02:15 +0300

    ÄÃ

---
+++ hello.py
@@ -0,0 +1,48 @@
+# phase 1
+#import math
+#import numbers
+#import random
+
+#name = input("What is your name? ")
+#print("Hello " + name + "!")
+
+# phase 2
+#radius = float(input("give the radius?"))
+#area = math.pi * radius ** 2
+#print("Area is " + str(area))
+#print(f"Area:Area is {area:.1f}")
+
+#phase 6
+
+#print(f"{random.randint(0, 999):03d}")
+#print(str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9)))
+
+#phase 3
+
+#lenght = float(input("give the length of the rectangle "))
+#width = float(input("give the width of the rectangle "))
+#perimeter = 2*(lenght + width)
+#area = lenght * width
+#print("The perimeter of the rectangle is " + str(perimeter) + (" and ") + "The area the rectangle is " + str(area))
+
+#phase 4
+
+#num1 = int(input("Enter 1st number "))
+#num2 = int(input("Enter 2nd  number "))
+#num3 = int(input("Enter 3rd number "))
+#sum = num1+num2+num3
+#product = num1*num2*num3
+#average = (sum)/3
+#print("Give me three integer number than I will give you the sum, the product and the average of the three number ")
+#print("the sum is " + str(sum) + " the product is " + str(product) + " and the average is " + str(average)
+
+secret_code1 = "NATO"
+secret_code2 = "EU"
+answer = input("what is the secret code?")
+
+if answer == secret_code1
+   print("correct!")
+elif answer == secret_code2:
+   print("Also correct")
+else:
+   print("not correct")
