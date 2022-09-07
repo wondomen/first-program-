2ac816a6066a201782cb7aa4305169c22d48d745
wondo <Muche.Berhanu@metropolia.fi>
2022-09-07 23:08:53 +0300

    doing exercise practice

---
+++ Exercies 4.py
@@ -0,0 +1,70 @@
+# Question 1
+#n = 1
+#while (n <= 1000):
+#    if (n%3 == 0):
+ #       print(int(n))
+  #  n = n + 1
+# Question 2
+
+#length_inch = float(input("Enter the length in inch "))
+#length_cm = 2.54 * length_inch
+#while length_inch > 0:
+ #   print(length_cm)
+  #  length_inch = float(input("Enter the length in inch "))
+
+# Question 3
+
+#largest = None
+#smallest = None
+#while True:
+#    num = input("Enter a number: ")
+#    if num == 'done':
+  #      break
+   # try:
+    #    n = int(num)
+    #except:
+    #    print('Invalid input')
+    #    continue
+    #if largest is None or largest < n:
+     #   largest = n
+    #elif smallest is None or smallest > n:
+    #    smallest = n
+    #elif n < smallest:
+     #   smallest = n
+#print('Largest is',largest)
+#print('Smallest is',smallest)
+
+
+ # Question 4
+
+#import random
+#number = random.randint(1, 10)
+#attempts = 0
+#guess = 0
+#while guess != number:
+#    guess = eval(input("Guess a number: "))
+#    attempts = 1 + attempts
+#    if guess == number:
+#        print("Correct")
+#        break
+#    elif guess > number:
+#        print("Too high")
+#    else:
+#        print("Too low")
+
+# Question 5
+
+#print("username and password")
+#count = 0
+#while count < 5:
+#    username = input("Enter username ")
+#    password = input("Enter pass word ")
+#    if username == "python" and password == "rules" and count < 5:
+#        print("Welcome")
+#        break
+#    else:
+#        print("Access denied ")
+#        count = 1 + count
+
+#Question 6
+
