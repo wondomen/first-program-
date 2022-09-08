8e8e21db2926e2d5b4b8e1df51913810b2488e88
wondo <Muche.Berhanu@metropolia.fi>
2022-09-08 15:02:30 +0300

    doing exercise practice

---
+++ Exericise 4 while loop .py
@@ -0,0 +1,103 @@
+import math
+import random
+# Question 1
+
+n = 1
+while  n <= 1000:
+    if n%3 == 0:
+        print(int(n))
+    n = n + 1
+
+# Question 2
+
+length_inch = float(input("Enter the length in inch "))
+length_cm = 2.54 * length_inch
+while length_inch >= 0:
+    print(length_cm)
+    length_inch = float(input("Enter the length in inch "))
+
+# Question number 3
+
+
+largest = None
+smallest = None
+while True:
+    num = input("Enter a number: ")
+    if num == "":
+        break
+    try:
+        n = int(num)
+    except:
+        print('Invalid input')
+        continue
+    if largest is None or largest < n:
+      largest = n
+    elif smallest is None or smallest > n:
+      smallest = n
+    elif n < smallest:
+        smallest = n
+print('Largest is',largest)
+print('Smallest is',smallest)
+prompt = "Give a number?"
+s= input(prompt)
+if s != "":
+    smallest = int(s)
+    largest = smallest
+while s != "":
+    n = int(s)
+    if n < smallest:
+        smallest = n
+    if n > largest:
+        largest = n
+    s = input(prompt)
+else:
+    print("the largest is " , largest)
+    print('smallest is ', smallest)
+
+# Question 4
+
+import random
+number = random.randint(1, 10)
+attempts = 0
+guess = 0
+while guess != number:
+    guess = eval(input("Guess a number: "))
+    attempts = 1 + attempts
+    if guess == number:
+        print("Correct")
+        break
+    elif guess > number:
+        print("Too high")
+    else:
+        print("Too low")
+
+
+# Question 5
+
+print("username and password")
+count = 0
+while count < 5:
+    username = input("Enter username ")
+    password = input("Enter pass word ")
+    if username == "python" and password == "rules" and count < 5:
+        print("Welcome")
+        break
+    else:
+        print("Access denied ")
+        count = 1 + count
+
+#Question 6
+
+import random
+n = 0
+count = float(input("Enter how many random points to generate?  "))
+inside = 0
+while n != count:
+    rand_x = random.uniform(-1,1)
+    rand_y = random.uniform(-1,1)
+    n = n + 1
+
+    if (rand_x**2) + (rand_y**2) < 1:
+        inside = inside + 1
+pi = float(4 * inside / count)
+print('the approximate value of pi is:', pi)
